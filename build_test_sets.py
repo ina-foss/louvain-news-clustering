4e71df3036be2603b6584fc1dab410d968ea36f8
bmaz <beatrice.mazoyer@gmail.com>
2020-05-20 22:20:37 +0200

    build_test_sets.py

--- build_test_sets.py
+++ build_test_sets.py
@@ -21,8 +21,22 @@ news["date"] = news["created_at"].apply(get_date)
 news = news.sort_values("date").reset_index(drop=True)
 
 window = int(tweets.shape[0]/4)
-for i in range(0, tweets.shape[0], window):
+for enum, i in enumerate(range(0, tweets.shape[0], window)):
+    print(enum)
     short = tweets[i:i+window]
     max_date = datetime.strptime(short.loc[i + window - 1].created_at, ES_DATE_FORMAT)
     if i == 0:
         short_news = news[news.date <= max_date]
+    elif enum in [2, 4]:
+        min_date = datetime.strptime(short.loc[i].created_at, ES_DATE_FORMAT)
+        short_news = news[(news.date <= max_date) & (news.date > min_date)]
+    else:
+        min_date = datetime.strptime(short.loc[i].created_at, ES_DATE_FORMAT)
+        short_news = news[news.date > min_date]
+
+    short.to_csv("/home/bmazoyer/Dev/news_twitter/data/event2018_short_{}_tweets.tsv".format(enum), sep="\t",
+                 quoting=csv.QUOTE_ALL,
+                 index=False)
+    short_news.to_csv("/home/bmazoyer/Dev/news_twitter/data/event2018_short_{}_news.tsv".format(enum), sep="\t",
+                 quoting=csv.QUOTE_ALL,
+                 index=False)
